6a294ce5be76cda159cfde76117a347cde8fbd61
sofia-abalos <sofianinasofia@gmail.com>
2023-05-24 12:09:44 -0300

    Creamos el primer commit del proyecto

---
+++ sketch.js
@@ -0,0 +1,176 @@
+function setup() {
+    console.log("Composition in dissonances (1919) by Theo van Doesburg / Trabajo   realizado por Sofia N. Abalos Miguez"); 
+    createCanvas(400, 500);
+    background(255);
+    strokeWeight(2);
+    //1-1 rec
+    fill(190, 180, 173); //lila
+    rect(0, 0, 90, 30);
+    //1-2 rec
+    fill(190, 145, 14);  //mostaza
+    rect(90, 0, 50, 30);
+    //1-3 rec
+    fill(221, 182, 176); //rosa
+    rect(140, 0, 80, 30);
+    //1-4 rec
+    fill(190, 180, 173); //lila
+    rect(220, 0, 40, 80);
+    //1-5 rec
+    fill(221, 182, 176); //rosa
+    rect(260, 0, 70, 30);
+    //1-6 rec
+    fill(141, 180, 149); //verde
+    rect(330, 0, 30, 80);
+    //1-7 rec
+    fill(108, 130, 167); //azul
+    rect(360, 0, 41, 140);
+    //2-1 rec
+    fill(141, 180, 149); //verde
+    rect(0, 30, 20, 110);
+    //2-2 rec
+    fill(108, 130, 167); //azul
+    rect(20, 30, 40, 110);
+    //2-3 rec
+    fill(141, 180, 149); //verde
+    rect(60, 30, 90, 50);
+    //2-4 rec
+    fill(108, 130, 167); //azul
+    rect(150, 30, 70, 50);
+    //2-5 rec
+    fill(190, 145, 14);  //mostaza
+    rect(260, 30, 70, 50);
+    //3-1 rec 
+    fill(190, 180, 173);  //lila
+    rect(60, 80, 40, 90);
+    //3-2 rec
+    fill(221, 182, 176); //rosa
+    rect(100, 80, 40, 90);
+    //3-3 rec
+    fill(190, 145, 14);  //mostaza
+    rect(140, 80, 50, 60);
+    //3-4 rec
+    fill(141, 180, 149); //verde
+    rect(190, 80, 60, 60);
+    //3-5 rec
+    fill(108, 130, 167); //azul
+    rect(250, 80, 40, 60);
+    //3-6 rec
+    fill(190, 180, 173);  //lila
+    rect(290, 80, 70, 60);
+    //4-1 rec
+    fill(190, 145, 14);  //mostaza
+    rect(0, 140, 20, 90);
+    //4-2 rec
+    fill(221, 182, 176); //rosa
+    rect(20, 140, 40, 90);
+    //4-3 rec
+    fill(108, 130, 167); //azul
+    rect(140, 140, 60, 30);
+    //4-4 rec
+    fill(190, 180, 173);  //lila
+    rect(200, 140, 50, 60);
+    //4-5 rec
+    fill(221, 182, 176); //rosa
+    rect(250, 140, 70, 60);
+    //4-6 rec
+    fill(190, 145, 14);  //mostaza
+    rect(320, 140, 81, 60);
+    //5-1 rec
+    fill(108, 130, 167); //azul
+    rect(60, 170, 40, 60);
+    //5-2 rec
+    fill(190, 145, 14);  //mostaza
+    rect(100, 170, 60, 60);
+    //5-3 rec
+    fill(141, 180, 149); //verde
+    rect(160, 170, 40, 60);
+    //6-1 rec
+    fill(190, 145, 14);  //mostaza
+    rect(200, 200, 80, 60);
+    //6-2 rec
+    fill(190, 180, 173);  //lila
+    rect(280, 200, 60, 90);
+    //6-3 rec
+    fill(221, 182, 176); //rosa
+    rect(340, 200, 61, 90);
+    //7-1 rec
+    fill(190, 180, 173);  //lila
+    rect(0, 230, 60, 60);
+    //7-2 rec
+    fill(141, 180, 149); //verde
+    rect(60, 230, 40, 60);
+    //7-3 rec
+    fill(221, 182, 176); //rosa
+    rect(100, 230, 60, 60);
+    //7-4 rec
+    fill(190, 180, 173);  //lila
+    rect(160, 230, 40, 60);
+    //8-1 rec
+    fill(221, 182, 176); //rosa
+    rect(200, 260, 50, 90);
+    //8-2 rec
+    fill(141, 180, 149); //verde
+    rect(250, 260, 30, 90);
+    //9-1 rec
+    fill(221, 182, 176); //rosa
+    rect(0, 290, 60, 80);
+    //9-2 rec
+    fill(190, 145, 14);  //mostaza
+    rect(60, 290, 30, 80);
+    //9-3 rec
+    fill(108, 130, 167); //azul
+    rect(90, 290, 50, 60);
+    //9-4 rec
+    fill(141, 180, 149); //verde
+    rect(140, 290, 60, 60);
+    //9-5 rec
+    fill(108, 130, 167); //azul
+    rect(280, 290, 60, 60);
+    //9-6 rec
+    fill(190, 145, 14);  //mostaza
+    rect(340, 290, 61, 60);
+    //10-1 rec
+    fill(221, 182, 176); //rosa
+    rect(90, 350, 70, 70);
+    //10-2 rec
+    fill(190, 180, 173);  //lila
+    rect(160, 350, 70, 70);
+    //10-3 rec
+    fill(190, 145, 14);  //mostaza
+    rect(230, 350, 50, 70);
+    //10-4 rec
+    fill(221, 182, 176); //rosa
+    rect(280, 350, 50, 120);
+    //10-5 rec
+    fill(190, 180, 173);  //lila
+    rect(330, 350, 45, 120);
+    //10-6 rec
+    fill(141, 180, 149); //verde
+    rect(375, 350, 26, 120); 
+    //11-1 rec
+    fill(190, 180, 173);  //lila
+    rect(0, 370, 45, 100);
+    //11-2 rec
+    fill(141, 180, 149); //verde
+    rect(45, 370, 45, 100);
+    //12-1 rec
+    fill(190, 145, 14);  //mostaza
+    rect(90, 420, 70, 50);
+    //12-2 rec
+    fill(141, 180, 149); //verde
+    rect(160, 420, 50, 91);
+    //12-2 rec
+    fill(108, 130, 167); //azul
+    rect(210, 420, 70, 91);
+    //13-1 rec
+    fill(108, 130, 167); //azul
+    rect(0, 470, 90, 31);
+    //13-2 rec
+    fill(190, 180, 173);  //lila
+    rect(90, 470, 70, 31);
+    //13-3 rec
+    fill(190, 145, 14);  //mostaza
+    rect(280, 470, 121, 31);
+    
+  }
+  
